2577a9fea655e894bd4a3cfba4ce9c5d64759ee2
Greg Robertson <grobertson743@gmail.com>
2023-02-02 14:02:32 -0800

    config vite/eslint

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, ReactDOM from "react";
+
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Hunter",
+      breed: "Golden Retriever",
+    }),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Saint",
+      breed: "Border Collie",
+    }),
+    React.createElement(Pet, {
+      animal: "Bunny",
+      name: "Thumper",
+      breed: "Long-haired",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
