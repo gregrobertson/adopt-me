34a7204825d6e5062b00f2a52390935ed22347bc
Greg Robertson <grobertson743@gmail.com>
2023-02-02 13:07:11 -0800

    props down to children

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+
+
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
+    ])
+}
+
 const App = () => {
     return React.createElement(
         "div",
         {},
-        React.createElement("h1", {}, "Adopt Me!")
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet, {
+                animal: "Dog",
+                name: "Hunter",
+                breed: "Golden Retriever"
+            }),
+            React.createElement(Pet, {
+                animal: "Dog",
+                name: "Saint",
+                breed: "Border Collie"
+            }),
+            React.createElement(Pet, {
+                animal: "Bunny",
+                name: "Thumper",
+                breed: "Long-haired"
+            }),
+        ]
     )
 };
 
